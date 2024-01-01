b0ad2ab624e8a78b155b38a45cf101dd559f8ca0
Arun-webcode <vikratkumarprajapati4@gmail.com>
2024-01-01 19:24:17 +0530

    added new problem middleware rqst login

---
+++ Express server middleware-Request login/index.js
@@ -0,0 +1,21 @@
+// Please don't change the pre-written code.
+
+const express = require("express");
+const app = express();
+
+const logRequest = () => {
+  // Write your code here
+  app.get("/", (req, res, next) => {
+    console.log(req.path);
+    console.log(req.method);
+    next();
+  });
+};
+
+logRequest();
+
+app.get("/", (req, res) => {
+  res.send("Coding Ninjas!");
+});
+
+module.exports = app;
