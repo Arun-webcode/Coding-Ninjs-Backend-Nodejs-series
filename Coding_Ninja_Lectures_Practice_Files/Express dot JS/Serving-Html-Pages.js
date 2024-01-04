b2b5501ab7afdf7708ff5370f63e27d7c3cdc82b
Arun-webcode <vikratkumarprajapati4@gmail.com>
2024-01-04 19:47:35 +0530

    added problems

--- Coding_Ninja_Lectures_Practice_Files/Express dot JS/Serving-Html-Pages.js
+++ Coding_Ninja_Lectures_Practice_Files/Express dot JS/Serving-Html-Pages.js
@@ -3,8 +3,11 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Welcome to Express");
+  return res.send("Welcome to Express");
 });
+
+// my static files are in public folder which can be accessed directly
+server.use(express.static("public"));
 
 server.listen(3300);
 
---
+++ Custom Header Setter for HTTP Response/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const server = express();
+
+// Set custom header on response object
+const setCustomHeader = (res, headerName, headerValue) => {
+  res.set(headerName, headerValue);
+  console.log(
+    `${headerName} with value ${headerValue} has been set successfully!`
+  );
+};
+
+// Route that uses the setCustomHeader function
+server.get("/", (req, res) => {
+  setCustomHeader(res, "Content-Type", "application/json");
+  res.send(`get method called!`);
+});
+
+module.exports = { setCustomHeader, server };
